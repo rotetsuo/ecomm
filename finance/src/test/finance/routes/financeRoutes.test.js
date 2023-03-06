02861fb749c5a970fb79aca51262bb4805c0d319
HELNEY ROBERTO TETSUO HARADA <n917700@gruposantander.com>
2023-03-06 17:03:32 -0300

    Testes Finance semana 8

--- finance/src/test/finance/routes/financeRoutes.test.js
+++ finance/src/test/finance/routes/financeRoutes.test.js
@@ -1,6 +1,7 @@
 const request = require ('supertest');
 const {describe, expect, it} = require ('@jest/globals');
 const app = require('../../../app')
+
 let idResposta;
 describe('POST /payments', ()=> {
     it('cria um pagamento', async ()=> {
@@ -25,17 +26,3 @@ describe('GET /payments/:id', () => {
         .expect(200);
     });
   });
-describe('PUT em /payments/:id', () => {
-    test.each([
-        ['cardName', { cardName: 'LAYSSATESTE' }],
-        ['value', { value: 999.99 }],
-    ])('Deve alterar o campo %s', async (chave, param) => {
-        const requisicao = { request };
-        const spy = jest.spyOn(requisicao, 'request');
-        await requisicao.request(app)
-        .put(`/payments/${idResposta}`)
-        .send(param)
-        .expect(200);
-        expect(spy).toHaveBeenCalled();
-    });
-});
