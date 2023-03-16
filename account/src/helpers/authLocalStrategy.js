b04772a2bf1d5211ec1eb8e787912e8f45345f11
HELNEY ROBERTO TETSUO HARADA <n917700@gruposantander.com>
2023-03-16 11:54:40 -0300

    local strategy

---
+++ account/src/helpers/authLocalStrategy.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import { Strategy as LocalStrategy } from 'passport-local';
+
+import userModel from '../models/User.js';
+import { verifyPassword } from './bcryptHash.js';
+
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'senha',
+        session: false
+    }, async (email, senha, done) => {
+        try {
+            const user = await userModel.findOne({ email });
+            if (!user) throw new Error('Nenhum usuário encontrado');
+
+            if (!verifyPassword(senha, user.senha)) {
+              throw new Error('Email ou senha inválidos');
+            }
+
+            done(null, user);
+        } catch (error) {
+            done(error);
+        }
+    })
+);
